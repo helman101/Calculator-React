2390d88a741f475e3898eda75a39fda1f1053dbf
helman101 <helmanandresortegon@gmail.com>
2021-05-11 11:36:49 +0200

    add operate method to operate.js file

---
+++ src/logic/operate.js
@@ -0,0 +1,12 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  switch(operation) {
+    case '-':
+        Big(numberOne) - Big(numberTwo)
+      break;
+    case y:
+      // code block
+      break;
+  }
+}
