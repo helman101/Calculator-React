84a9cc60a4a71daf3f5d93b22ca2c35ccdd721fe
helman101 <helmanandresortegon@gmail.com>
2021-05-11 11:28:20 +0200

    add calculate method in calculate.js

---
+++ src/logic/calculate.js
@@ -0,0 +1,11 @@
+const calculate = ({total, next, operation}, buttonName) => {
+  switch(buttonName) {
+    case '+/-':
+        total *= -1
+        next *= -1
+      break;
+    case y:
+      // code block
+      break;
+  }
+}
