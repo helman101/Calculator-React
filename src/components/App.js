48c93cba9c09f775733bf496dcee898384e1a525
helman101 <helmanandresortegon@gmail.com>
2021-05-11 12:13:35 +0200

    implement TSE changes in milestone-2

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,13 @@ import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      result: '0',
-    };
-  }
-
-  render() {
-    const { result } = this.state;
-    return (
-      <div>
-        <Display result={result} />
-        <ButtonPanel />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
+  </div>
+);
 
 export default App;
