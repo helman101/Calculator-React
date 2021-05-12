ae814a7e3003cfb1c5c032061bbdca5b52992e57
helman101 <helmanandresortegon@gmail.com>
2021-05-12 10:56:25 +0200

    change App function component to clas component and add state

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 /* import calculate from '../logic/calculate'; */
 
-const App = () => (
-  <div>
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    }
+  }
+}
 
 export default App;
